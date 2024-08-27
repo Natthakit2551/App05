0e5b7bed04bb383f0c2be2f9c86b55e9e4e576a8
Natthakit Lekmas <natthakit.lekmas@gmail.com>
2024-08-27 14:28:12 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,5 +8,35 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  item=[
+    {
+      id:1,
+      Name:'product A' ,
+      price : '200',
+      detail: 'this is a product A',
+      imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4WfjYEh0hyIW8Q3834PjSiT-vdOkEnOy99g&s',
+    },
+    {
+      id:2,
+      Name:'product B' ,
+       price : '250',
+      detail: 'this is a product B' ,
+      imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHF5m_uswGtS8qpr13lzE4kJweXGbepX9EoA&s',
+    },
+    {
+      id:3,
+      Name:'product C' ,
+      price : '300',
+      detail: 'this is a product C' ,
+      imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwAirk1HW0E4NrkKThwwC9Zm4chu6EhTQAXQ&s',
+    },
+    {
+      id:4,
+      Name:'product D' ,
+      price : '350',
+      detail: 'this is a product D' ,
+      imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8aUgi5u8uZNyFTd54Rbzs4rVWOI_VfIea5w&s',
+    },
+  ]
 
 }
